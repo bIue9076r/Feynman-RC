d869332da4532ae6ec950ff2f4a947c32cbf9a10
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-09-12 23:50:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
+#include <Arduino.h>
+
 void setup() {
-	
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-	
+void loop(){
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
 }
 
 int main(void){
+	init();
 	setup();
+
 	for(;;){
 		loop();
 	}
-	
+
 	return 0;
 }
